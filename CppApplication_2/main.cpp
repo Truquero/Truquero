4cd5e4fa6cae45ff41eb4a236c78653975a98a1a
Truquero <acticj@gmail.com>
2024-09-24 17:46:50 +0200

    Hola

---
+++ CppApplication_2/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/main.cc to edit this template
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: actci
+ *
+ * Created on 24 de septiembre de 2024, 17:39
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "Hello";
+    return 0;
+}
+
